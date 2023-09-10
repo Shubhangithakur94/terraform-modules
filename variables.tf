65001b8055783f0b5d0a76bc104d8b1d77082b66
Shubhangi thakur <99044867+Shubhangithakur94@users.noreply.github.com>
2023-09-10 15:46:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "router_name" {}
+variable "cloudnat_region" {}
+variable "bgp_asn" {}
+variable "nat_name" {}
+variable "nat_ip_allocate_option" {}
+variable "source_subnetwork_ip_ranges_to_nat" {}
+variable "ip_count" {}
